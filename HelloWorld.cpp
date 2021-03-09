2f4497261787500e6582fcebd118156751d91a5a
Michael Banner <mbanner3@email.cpcc.edu>
2021-03-09 10:11:29 -0500

    Added Notes
    Add notes to program

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,9 @@
+// Program modified, but able to revert to original version using GitHub Desktop.
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	cout << "Hello world!" << endl;
-	cout << "Program modified, but able to revert to original version using GitHub Desktop" << endl;
 	return 0;
 }
